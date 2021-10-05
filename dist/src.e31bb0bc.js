85e77cdc87a57f1b1e72e01742901145add71fea
George Lubenets <glubenets@ds.do>
2021-10-05 17:52:42 +0300

    chat-list structure init

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11398" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12342" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
